11ee06fdf29e5b5baddc933baa17e4cb1f59d325
Sergei <sdonich@gmail.com>
2019-10-03 14:39:41 +0200

    Fin

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,10 @@
-interface Mappble {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
-  }
+  };
+  markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
@@ -18,13 +20,20 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: Mappble): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
--- src/User.ts
+++ src/User.ts
@@ -1,18 +1,23 @@
-import faker from 'faker';
+import faker from "faker";
+import { Mappable } from "./CustomMap";
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
 
   constructor() {
     this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
-    }
+    };
+  }
+
+  markerContent(): string {
+    return `User name: ${this.name}`;
   }
 }
-
