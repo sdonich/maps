3865e485eb04b01c2d26cd3ccc916e4a5f446764
Sergei <sdonich@gmail.com>
2019-10-03 13:35:50 +0200

    Add CustomMap.ts and style.css

---
+++ src/CustomMap.ts
@@ -0,0 +1,30 @@
+interface Mappble {
+  location: {
+    lat: number;
+    lng: number;
+  }
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappble): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+  }
+}
